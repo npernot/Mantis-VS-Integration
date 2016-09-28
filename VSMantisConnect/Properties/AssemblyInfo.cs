cc44ab030343aab148cf699fa4a77ae4c6d1b4ad
Nicolas PERNOT <nicolas.pernot@fwa.eu>
2016-09-28 11:02:41 +0200

    Ajout du fichier de spécification Nuget Mise à jour des infos d'assembly

--- VSMantisConnect/Properties/AssemblyInfo.cs
+++ VSMantisConnect/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VSMantisConnect")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("VS Mantis Connect")]
+[assembly: AssemblyDescription("Connect to Mantis Bug Tracker")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VSMantisConnect")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyProduct("VS Mantis Connect")]
+[assembly: AssemblyCopyright("2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,3 +32,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
